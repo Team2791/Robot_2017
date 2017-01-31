f966fe1f086a4643578d0749ddb025c595931d5c
Akhil Jacob <akhilcjacob@gmail.com>
2017-01-31 17:52:02 -0500

    drivetrain code moved

--- src/org/usfirst/frc/team2791/robot/subsystems/ShakerDrivetrain.java
+++ src/org/usfirst/frc/team2791/robot/subsystems/ShakerDrivetrain.java
@@ -25,13 +25,13 @@ public class ShakerDrivetrain extends Subsystem{
 	
     protected RobotDrive shakyDrive = null;
 
-    private Talon leftTalonA;
-    private Talon leftTalonB;
-    private Talon leftTalonC;
+    private Talon leftSparkA;
+    private Talon leftSparkB;
+    private Talon leftSparkC;
     
-    private Talon rightTalonA;
-    private Talon rightTalonB;
-    private Talon rightTalonC;
+    private Talon rightSparkA;
+    private Talon rightSparkB;
+    private Talon rightSparkC;
     
     public void initDefaultCommand() {
 	//is there where I declare a drivetrain or will it reset a drivetrain every single time?
@@ -39,21 +39,21 @@ public class ShakerDrivetrain extends Subsystem{
 	// Set the default command for a subsystem here.
 	// setDefaultCommand(new MySpecialCommand());
 		
-	this.leftTalonA = new Talon(RobotMap.DRIVE_TALON_LEFT_PORT);
-    this.leftTalonB = new Talon(RobotMap.DRIVE_TALON_LEFT_PORT);
-    this.leftTalonC = new Talon(RobotMap.DRIVE_TALON_LEFT_PORT);
+	this.leftSparkA = new Talon(RobotMap.DRIVE_TALON_LEFT_PORT);
+    this.leftSparkB = new Talon(RobotMap.DRIVE_TALON_LEFT_PORT);
+    this.leftSparkC = new Talon(RobotMap.DRIVE_TALON_LEFT_PORT);
 
-    this.rightTalonA = new Talon(RobotMap.DRIVE_TALON_RIGHT_PORT);
-    this.rightTalonB = new Talon(RobotMap.DRIVE_TALON_RIGHT_PORT);
-    this.rightTalonC = new Talon(RobotMap.DRIVE_TALON_RIGHT_PORT);
+    this.rightSparkA = new Talon(RobotMap.DRIVE_TALON_RIGHT_PORT);
+    this.rightSparkB = new Talon(RobotMap.DRIVE_TALON_RIGHT_PORT);
+    this.rightSparkC = new Talon(RobotMap.DRIVE_TALON_RIGHT_PORT);
 
     this.leftDriveEncoder = new Encoder(RobotMap.LEFT_DRIVE_ENCODER_PORT_A, RobotMap.LEFT_DRIVE_ENCODER_PORT_B);
     this.rightDriveEncoder = new Encoder(RobotMap.RIGHT_DRIVE_ENCODER_PORT_A,
             RobotMap.RIGHT_DRIVE_ENCODER_PORT_B);
     
     // use the talons to create a roboDrive (it has methods that allow for easier control)
-    this.shakyDrive = new RobotDrive(new TalonSet(leftTalonA, leftTalonB, leftTalonC),
-            new TalonSet(rightTalonA, rightTalonB, rightTalonC));
+    this.shakyDrive = new RobotDrive(new TalonSet(leftSparkA, leftSparkB, leftSparkC),
+            new TalonSet(rightSparkA, rightSparkB, rightSparkC));
     //robotDrive = new RobotDrive(leftTalonA, leftTalonB, rightTalonA, rightTalonB);
     // stop all motors right away just in case
     shakyDrive.stopMotor();
@@ -69,7 +69,7 @@ public class ShakerDrivetrain extends Subsystem{
 		
 	}
 	
-    public void driveWithJoystick(){
+   /* public void driveWithJoystick(){
 	    //logic interprets driver Joystick position for motor outputs 
 	    double combinedLeft, combinedRight;
 	    //if we need to change the speed we can change the .35 FIRST and then the /3 ONLY if thats not enough
@@ -85,11 +85,11 @@ public class ShakerDrivetrain extends Subsystem{
 	 	combinedLeft=Robot.oi.driver.getGtaDriveLeft();
 		combinedRight=Robot.oi.driver.getGtaDriveRight();
 	    }
-	    shakerDrive(combinedLeft,combinedRight);
-	}
+	    setLeftRightMotorOutputs(combinedLeft,combinedRight);
+	} */
 	
     //set motor output according to above interpretation
-    public void shakerDrive(double left, double right){
+    public void setLeftRightMotorOutputs(double left, double right){
     	shakyDrive.setLeftRightMotorOutputs(left, right);
     }
 	
