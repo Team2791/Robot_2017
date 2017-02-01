4b5f4f9234174c5d8b7617223c776457e615ccf5
Akhil Jacob <akhilcjacob@gmail.com>
2017-02-01 15:17:08 -0500

    commit #2

--- src/org/usfirst/frc/team2791/robot/subsystems/ShakerIntake.java
+++ src/org/usfirst/frc/team2791/robot/subsystems/ShakerIntake.java
@@ -1,5 +1,12 @@
 package org.usfirst.frc.team2791.robot.subsystems;
 
-public class ShakerIntake {
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Talon;
 
+public class ShakerIntake { //code for intake and climber
+	private Talon shooterSpark;
+	
+	protected Encoder shooterEncoder = null;
+	
+	
 }
