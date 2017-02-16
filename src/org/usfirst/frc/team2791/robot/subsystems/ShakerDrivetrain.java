fdf40ec51f7b6bd185bd2e09d9d27ac6756fc2f1
maherb <maherb@rpi.edu>
2017-02-16 18:20:52 -0500

    renamed leftSparkA to leftSpark and rightSparkA to rightSpark and removed extra spark declarations

--- src/org/usfirst/frc/team2791/robot/subsystems/ShakerDrivetrain.java
+++ src/org/usfirst/frc/team2791/robot/subsystems/ShakerDrivetrain.java
@@ -32,13 +32,8 @@ public class ShakerDrivetrain extends Subsystem{
 	
 	protected RobotDrive shakyDrive = null;
 
-	private Talon leftSparkA;
-	private Talon leftSparkB;
-	private Talon leftSparkC;
-	
-	private Talon rightSparkA;
-	private Talon rightSparkB;
-	private Talon rightSparkC;
+	private Talon leftSpark;
+	private Talon rightSpark;
 	
 	protected static BasicPID movingAnglePID;
 	protected static BasicPID distancePID;
@@ -65,14 +60,14 @@ public class ShakerDrivetrain extends Subsystem{
 		testSolenoid = new Solenoid(1,7);
 		System.out.println("the solenoid is alive");
 		
-		this.leftSparkA = new Talon(RobotMap.DRIVE_TALON_LEFT_PORT);
-		this.rightSparkA = new Talon(RobotMap.DRIVE_TALON_RIGHT_PORT);
+		this.leftSpark = new Talon(RobotMap.DRIVE_TALON_LEFT_PORT);
+		this.rightSpark = new Talon(RobotMap.DRIVE_TALON_RIGHT_PORT);
 		
 		this.leftDriveEncoder = new Encoder(RobotMap.LEFT_DRIVE_ENCODER_PORT_A, RobotMap.LEFT_DRIVE_ENCODER_PORT_B);
 		this.rightDriveEncoder = new Encoder(RobotMap.RIGHT_DRIVE_ENCODER_PORT_A,RobotMap.RIGHT_DRIVE_ENCODER_PORT_B);
 		
 		// use the talons to create a roboDrive (it has methods that allow for easier control)
-		this.shakyDrive = new RobotDrive(rightSparkA, leftSparkA);
+		this.shakyDrive = new RobotDrive(rightSpark, leftSpark);
 		//robotDrive = new RobotDrive(leftTalonA, leftTalonB, rightTalonA, rightTalonB);
 		// stop all motors right away just in case
 		shakyDrive.stopMotor();
