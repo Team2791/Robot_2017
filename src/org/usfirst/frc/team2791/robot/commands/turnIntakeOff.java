d3888ec471c66415b44500aec79ee7d753867a5c
maherb <maherb@rpi.edu>
2017-02-16 19:27:36 -0500

    Fixed brackets and spacing

--- src/org/usfirst/frc/team2791/robot/commands/turnIntakeOff.java
+++ src/org/usfirst/frc/team2791/robot/commands/turnIntakeOff.java
@@ -4,29 +4,33 @@ import org.usfirst.frc.team2791.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
 
-public class turnIntakeOff extends Command{
-	public turnIntakeOff(){
+public class turnIntakeOff extends Command {
+	public turnIntakeOff() {
 		super("turnIntakeOff");
 		requires(Robot.intake);// Use requires() here to declare subsystem dependencies
 		System.out.println("came to constructor of off-ing intake");
 		initialize();
 	}
-	protected void initialize(){
+
+	protected void initialize() {
 		System.out.println("came to initialize of off-ing intake");
 		execute();
 	}
-	protected void execute(){
+
+	protected void execute() {
 		System.out.println("I'm trying to execute intake off");
 		Robot.intake.moveIntakeOut(false);
 		Robot.intake.motorOffIntake();
-		
 	}
-	protected boolean isFinished(){
+
+	protected boolean isFinished() {
 		return false;
 	}
-	protected void end(){
+
+	protected void end() {
 //		Robot.intake.stopMotor();
 	}
-	protected void interrupted(){
+
+	protected void interrupted() {
 	}
 }
