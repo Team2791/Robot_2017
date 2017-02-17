8167556bfaf7a4c52281403df5748628f9772da0
maherb <maherb@rpi.edu>
2017-02-16 19:25:49 -0500

    Fixed brackets and spacing

--- src/org/usfirst/frc/team2791/robot/commands/runClimber.java
+++ src/org/usfirst/frc/team2791/robot/commands/runClimber.java
@@ -6,31 +6,36 @@ import org.usfirst.frc.team2791.robot.Robot;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class runClimber extends Command{
-	public runClimber(){
+	public runClimber() {
 		super("runClimber");
 		requires(Robot.intake);// Use requires() here to declare subsystem dependencies
 		System.out.println("came to constructor of climber");
 		initialize();
 	}
-	protected void initialize(){
+
+	protected void initialize() {
 		Robot.intake.wingDeployment();
 		System.out.println("came to initialize of climber");
 		execute();
 	}
-	protected void execute(){
-		if((Robot.intake.getClimberCurrent()<30.0) && !(OI.operator.getButtonB())){
+
+	protected void execute() {
+		if ((Robot.intake.getClimberCurrent()<30.0) && !(OI.operator.getButtonB())) {
 			System.out.println("I'm trying to execute climbing\tClimber Current: "+Robot.intake.getClimberCurrent());
 			Robot.intake.motorOnClimber();
 		}
 		else
 			Robot.intake.stopClimber();
 	}
-	protected boolean isFinished(){
+
+	protected boolean isFinished() {
 		return false;
 	}
-	protected void end(){
+
+	protected void end() {
 		Robot.intake.stopClimber();
 	}
-	protected void interrupted(){
+
+	protected void interrupted() {
 	}
 }
