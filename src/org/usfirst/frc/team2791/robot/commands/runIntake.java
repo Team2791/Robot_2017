3a784c3ee795e0765a38503182d10b6fa255cb2a
maherb <maherb@rpi.edu>
2017-02-16 19:26:19 -0500

    Fixed brackets and spacing

--- src/org/usfirst/frc/team2791/robot/commands/runIntake.java
+++ src/org/usfirst/frc/team2791/robot/commands/runIntake.java
@@ -5,28 +5,32 @@ import org.usfirst.frc.team2791.robot.Robot;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class runIntake extends Command{
-	public runIntake(){
+	public runIntake() {
 		super("runIntake");
 		requires(Robot.intake);// Use requires() here to declare subsystem dependencies
 		System.out.println("came to constructor of intake");
 		initialize();
 	}
-	protected void initialize(){
+
+	protected void initialize() {
 		System.out.println("came to initialize of intake");
 		execute();
 	}
-	protected void execute(){
+
+	protected void execute() {
 		System.out.println("I'm trying to execute intake on\tIntake Current: "+Robot.intake.getClimberCurrent());
 		Robot.intake.moveIntakeOut(true);
 		Robot.intake.motorOnIntake();
-		
 	}
-	protected boolean isFinished(){
+
+	protected boolean isFinished() {
 		return false;
 	}
-	protected void end(){
+
+	protected void end() {
 		Robot.intake.stopClimber();
 	}
-	protected void interrupted(){
+
+	protected void interrupted() {
 	}
 }
