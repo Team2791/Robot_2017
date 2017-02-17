460b3e94a8c665b5acc3cf77f886db5e2f4ad2e3
maherb <maherb@rpi.edu>
2017-02-16 19:27:02 -0500

    Fixed brackets and spacing

--- src/org/usfirst/frc/team2791/robot/commands/runIntakeBelt.java
+++ src/org/usfirst/frc/team2791/robot/commands/runIntakeBelt.java
@@ -4,28 +4,32 @@ import org.usfirst.frc.team2791.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
 
-public class runIntakeBelt extends Command{
-	public runIntakeBelt(){
+public class runIntakeBelt extends Command {
+	public runIntakeBelt() {
 		super("runIntakeBelt");
 		requires(Robot.intake);// Use requires() here to declare subsystem dependencies
 		System.out.println("came to constructor of intake belts");
 		initialize();
 	}
-	protected void initialize(){
+
+	protected void initialize() {
 		System.out.println("came to initialize of intake belts");
 		execute();
 	}
-	protected void execute(){
+
+	protected void execute() {
 		System.out.println("I'm trying to execute intake belt");
 		Robot.intake.motorOnIntake();
-		
 	}
-	protected boolean isFinished(){
+
+	protected boolean isFinished() {
 		return false;
 	}
-	protected void end(){
+
+	protected void end() {
 		Robot.intake.stopClimber();
 	}
-	protected void interrupted(){
+
+	protected void interrupted() {
 	}
 }
