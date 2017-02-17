3944558b3b3091c01c169c6225f5381336e575cf
maherb <maherb@rpi.edu>
2017-02-16 19:24:28 -0500

    Fixed brackets and spacing

--- src/org/usfirst/frc/team2791/robot/commands/removeGear.java
+++ src/org/usfirst/frc/team2791/robot/commands/removeGear.java
@@ -5,26 +5,29 @@ import org.usfirst.frc.team2791.robot.Robot;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class removeGear extends Command{
-	public removeGear (){
+	public removeGear() {
 		super("removeGear");
 		requires(Robot.gearMechanism);// Use requires() here to declare subsystem dependencies
 		System.out.println("came to constructor of gear");
 		initialize();
 	}
-	protected void initialize(){
+
+	protected void initialize() {
 		System.out.println("came to initialize of gear");
 		execute();
 	}
-	protected void execute(){
+
+	protected void execute() {
 		System.out.println("I'm trying to execute gear removal");
 		Robot.gearMechanism.changeGearSolenoidState(true);
 	}
-	protected boolean isFinished(){
+	protected boolean isFinished() {
 		return false;
 	}
-	protected void end(){
-		
+
+	protected void end(){	
 	}
+
 	protected void interrupted(){
 	}
 }
