f62d1aeb3cfe4be991efd3d117145f21f1cfb44e
gbanerjee01 <gaurab.banerjee97@gmail.com>
2017-03-26 15:43:16 -0400

    gear subsystem comments

--- src/org/usfirst/frc/team2791/robot/subsystems/ShakerGear.java
+++ src/org/usfirst/frc/team2791/robot/subsystems/ShakerGear.java
@@ -8,7 +8,17 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+/**
+ * This class corresponds to the gear subsystem which uses pistons to actuate the scoring mechanism 
+ * and a bag motor on a Spark to run the roller intake. There are limit switches in the intake to automatically
+ * raise the intake once a gear is pulled in.
+ * 
+ * @author Gaurab Banerjee
+ * @author Unnas Hussain
+ *
+ */
 public class ShakerGear extends Subsystem{
+	
 	private Solenoid gearSolenoid;
 	private static boolean state;
 	
@@ -18,29 +28,47 @@ public class ShakerGear extends Subsystem{
 	public ShakerGear(){
 		gearSolenoid = new Solenoid(RobotMap.PCM_MODULE,RobotMap.GEAR_CHANNEL);
 		gearSpark = new Talon(RobotMap.GEAR_SPARK_PORT);
+		
 		limitSwitchLong = new DigitalInput(0);
 		limitSwitchShort = new DigitalInput(1);
 	}
+	
 	public void initDefaultCommand(){
 		gearSolenoid.set(false);
 	}
+	
 	public void runGearIntake(){
 		gearSpark.setSpeed(1.0);
 	}
+	
 	public void stopGearIntake(){
 		gearSpark.setSpeed(0.0);
 	}
+	
+	/**
+	 * 
+	 * @param state_ true = down / false = up
+	 */
 	public void changeGearSolenoidState(boolean state_){
 		state=state_;
 		gearSolenoid.set(state);
 	}
+	
+	/**
+	 * @return State of gear actuation solenoid. true = down / false = up
+	 */
 	public boolean getState(){
 		return state;
 	}
+	
+	/**
+	 * @return Summary state of limit switches in the intake. true = gear inside / false = no gear in intake
+	 */
 	public boolean getLimitSwitchState(){
-		//System.out.println("limit switch is "+limitSwitchLong.get());
+		//limit switches are all normally open which is why the values are returned inverted
 		return !limitSwitchLong.get() || !limitSwitchShort.get();
 	}
+	
 	public void debug(){
 		SmartDashboard.putBoolean("Gear Intake Status",getLimitSwitchState());
 		SmartDashboard.putBoolean("Gear state", gearSolenoid.get());
