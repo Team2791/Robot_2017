2ef796a550d05c5c30aee2a83dcc9b3eb31850e4
unbun <uwh.547@gmail.com>
2017-04-08 11:41:33 -0400

    Center Gear Auto Finalized

--- src/org/usfirst/frc/team2791/robot/commands/autos/pid/CenterGearAuton.java
+++ src/org/usfirst/frc/team2791/robot/commands/autos/pid/CenterGearAuton.java
@@ -1,8 +1,5 @@
 package org.usfirst.frc.team2791.robot.commands.autos.pid;
 
-import org.usfirst.frc.team2791.robot.commands.GearMechUp;
-import org.usfirst.frc.team2791.robot.commands.ScoreGearAutoReturn;
-
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
@@ -14,11 +11,9 @@ public class CenterGearAuton extends CommandGroup {
 	 * @param red  true if you are on the red side;  false if you are on the blue side
 	 */
 	public CenterGearAuton(String red) {
-    	addSequential(new DriveStraightEncoderGyro(-(110.5-36.0)/12, .7, 6));
+    	addSequential(new DriveStraightEncoderGyro(-(110.5-36.0)/12, .7, 6)); //110.5 worked
     	addSequential(new AutonGearScore());
     	addSequential(new DelayDrivetrain(1.0));
     	addSequential(new DriveStraightEncoderGyro(3.0, .7, 2));
-
-
     }
 }
