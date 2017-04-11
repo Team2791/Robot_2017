1dfa53f0502e9bc3b309bd7b03b37105106cca1c
Max Llewellyn <max.llewellyn@gmail.com>
2017-04-11 19:31:24 -0400

    This code calculates the image correctly but does not ensure a fresh image. This code also breaks when we loose the target.

--- src/org/usfirst/frc/team2791/robot/util/visionNetworkTable.java
+++ src/org/usfirst/frc/team2791/robot/util/visionNetworkTable.java
@@ -9,11 +9,14 @@ public class visionNetworkTable implements ITableListener {
 	private NetworkTable visionTargetsTable;
 	private AnalyzedContour[] foundContours = {};
 
-	public static double FOVX = 47;
-	public static double FOVY = 36.2;
-	public static double INCLINATION = 40;
-	public static int SIZEX = 320;
-	public static int SIZEY = 240;
+	private double FOVX = 47;
+	private  double FOVY = 36.2;
+	private double INCLINATION = 0;
+	private int SIZEX = 320;
+	private int SIZEY = 240;
+	
+	private boolean freshImage = false;
+	
 	
 	
 	public visionNetworkTable() {
@@ -22,10 +25,11 @@ public class visionNetworkTable implements ITableListener {
 		
 	}
 	
-	public double getBoilerAngleError() {	
+	public double getBoilerAngleError() {
 		double targetX = selectTarget().centerX;
 		double ndcX = 2 * targetX / SIZEX - 1;
 		double angle = Math.atan(Math.tan(Math.toRadians(FOVX / 2)) * ndcX);
+		angle = Math.toDegrees(angle);
 		double x = Math.sin(Math.toRadians(angle));
 		double z = Math.cos(Math.toRadians(angle));
 		z *= Math.cos(Math.toRadians(INCLINATION));
@@ -38,6 +42,7 @@ public class visionNetworkTable implements ITableListener {
 		synchronized (foundContours) {
 			possibleTargets = foundContours;
 		}
+		
 		try {
 			return possibleTargets[0];
 		} catch (IndexOutOfBoundsException e) {
@@ -75,17 +80,9 @@ public class visionNetworkTable implements ITableListener {
 		// set up an array to store our found targets
 		AnalyzedContour[] contourList = new AnalyzedContour[areas.length];
 		
-//		System.out.println("areas length = "+areas.length);
-//		System.out.println("centerYs length = "+centerYs.length);
-//		System.out.println("centerXs length = "+centerXs.length);
-//		System.out.println("heights length = "+heights.length);
-//		System.out.println("widths length = "+widths.length);
-//		System.out.println("soliditys length = "+soliditys.length);
-		
 		for(int i=0; i < areas.length; ++i) {
 			contourList[i] = new AnalyzedContour(areas[i], centerYs[i], centerXs[i], heights[i],
-					widths[i], soliditys[i]); 
-			
+					widths[i], soliditys[i]);
 		}
 		
 		return contourList;
